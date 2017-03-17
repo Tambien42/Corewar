a0a56b8676799496b34bbaa1faaa3fd8fbb8a9c4
Fabien CHUC <fchuc@e1r13p14.42.fr>
2017-03-17 17:39:07 +0100

    new test

--- test_asm.sh
+++ test_asm.sh
@@ -4,18 +4,26 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+#Path Champion
+DIR="resources/players/fail"
+
 # Create directory
 if [ -d log ]
 then rm -rf log
 fi
 mkdir log
 
+#delete .cor
+rm -f $DIR/*.cor
+
 # Assemble Champions
-for champion in ./resources/players/*.s
+for champion in $DIR/*.s
 do
 	name=$(basename "$champion" .s)
-	./resources/bin/asm_zaz $champion > ./log/${name}.zaz.log | mv ./resources/players/${name}.cor ./resources/players/${name}.zaz.cor
-	./resources/bin/asm $champion > ./log/${name}.log
+	echo $name
+	./resources/bin/asm_zaz $champion &> ./log/${name}.zaz.log
+	mv $DIR/${name}.cor $DIR/${name}.zaz.cor
+	./resources/bin/asm $champion &> ./log/${name}.log
 done
 
 # Compare asm output
@@ -36,11 +44,18 @@ do
 done
 
 # Compare champion binary files 
-for champion in ./resources/players/*.zaz.cor
+for champion in $DIR/*.zaz.cor
 do
+	rm -f champion chapion_zaz
 	name=$(basename "$champion" .zaz.cor)
-	hexdump -C ./resources/players/${name}.cor > champion
-	hexdump -C ./resources/players/${name}.zaz.cor > champion_zaz
+	hexdump -C $DIR/${name}.cor > champion
+	if [ !-f $DIR/${name}.cor ]
+		then touch champion
+	fi
+	if [ !-f $DIR/${name}.zaz.cor ]
+		then touch champion_zaz
+	fi
+	hexdump -C $DIR/${name}.zaz.cor > champion_zaz
 	cols=80
 	lines=0
 	clear
@@ -63,11 +78,6 @@ do
 		fi
 		lines=$(($lines+1))
  	done < champion 3<champion_zaz
-	read -n 1 -s -p "Press any key to continue"
+	read -n 1 -s -p "Press any key to continue $name"
 	printf "\n"
 done
-
-#Rappel
-#xxd -p $champion
-#od -t x1 -An ./resources/players/zork.cor
-#diff -W 180 -y champion champion_zaz | perl colordiff.pl | less -R
